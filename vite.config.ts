cfb3b234bed0fc48777b3f0e92f1cfc642af2408
Khliupnev Vladimir <proksik49032@mail.ru>
2026-01-19 20:18:46 +0400

    feat: add demo

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,17 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
+  base: '/vue-calendar/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue'],
+        }
+      }
+    }
+  }
 })
